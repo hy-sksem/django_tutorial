d8fe9c4c9839db6d5299f228c179a7e0a8f44e9a
hy-sksem <banbi.rice.cracker@gmail.com>
2021-07-10 12:16:55 +0900

    update/delete_key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=jlclrxvsrwr2x*+0)k03l4vqng!62s=*_v(3#=i7$l-^bax$n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
